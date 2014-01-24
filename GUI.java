c972eb27ade23838223f8541f7408e680783aa74
marckatz <marc.f.katz@gmail.com>
2014-01-23 23:20:45 -0500

    Revised GUI colors
    Changed pink to magenta and vice versa

--- GUI.java
+++ GUI.java
@@ -7,20 +7,22 @@ public class GUI extends JFrame implements ActionListener{
 
     private int turn;
     private GridLayout grid;
-    private Color[] colors = {Color.red, Color.blue, Color.green, Color.yellow, Color.pink, Color.darkGray};
+    private Color[] colors = {Color.red, Color.blue, Color.green, Color.yellow, Color.MAGENTA, Color.darkGray};
     private ArrayList<String> colorsS = new ArrayList<String>();
     private ArrayList<String> colorsN = new ArrayList<String>();
     private JButton[][] tempName;
     private Color selectedColor;
-    private int selectedColumn;    
+    private int selectedColumn;
+    private Color[][] board = new Color[10][4];
+    private Color[][] indicators = new Color[20][2];
 
     public JFrame initializeBoard(){
         turn = 1;
         tempName = new JButton[2][6];
         grid = new GridLayout(2,6);
-	selectedColor = Color.MAGENTA;
+	selectedColor = Color.PINK;
 	selectedColumn = -1;
-	String[] colorsA = {"RED", "BLUE", "GREEN", "YELLOW", "PINK", "DARK_GRAY"};
+	String[] colorsA = {"RED", "BLUE", "GREEN", "YELLOW", "MAGENTA", "DARK_GRAY"};
 	String[] colorsNA = {"1", "2", "3", "4"};
 	for(String s:colorsA){
 	    colorsS.add(s);
@@ -35,7 +37,7 @@ public class GUI extends JFrame implements ActionListener{
         panel.setLayout(grid);
         panel.setSize(500, 1000);
         frame.getContentPane().add(BorderLayout.CENTER, panel);
-        frame.setTitle("Mastermind");
+        frame.setTitle("Mastermind");	
 
 	tempName[0][0] = new JButton("CLEAR");
 	tempName[0][0].addActionListener(this);
@@ -46,7 +48,7 @@ public class GUI extends JFrame implements ActionListener{
 	for (int y = 1; y < 5; y++){
 	    tempName[0][y] = new JButton("" + y);
 	    tempName[0][y].addActionListener(this);
-	    tempName[0][y].setBackground(Color.MAGENTA);
+	    tempName[0][y].setBackground(Color.PINK);
 	    tempName[0][y].setActionCommand("" + y);
 	    panel.add(tempName[0][y]);
 	}
@@ -65,16 +67,6 @@ public class GUI extends JFrame implements ActionListener{
 	    panel.add(tempName[1][x]);
 	}
 
-	/*
-        for (int r = 0; r < 10; r++){
-	    for (int c = 0; c < 4; c++) {
-                button[r][c] = new JButton("Button");
-                button[r][c].addActionListener(this);
-                button[r][c].setBackground(Color.RED);
-                panel.add(button[r][c]);
-	    }
-	    }*/
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 1100);
         frame.setVisible(true);
@@ -92,17 +84,17 @@ public class GUI extends JFrame implements ActionListener{
 	    Integer i = new Integer(e.getActionCommand());
 	    selectedColumn = i.intValue();
 	}
-	if(selectedColumn != -1 && !(selectedColor.equals(Color.MAGENTA))){
+	if(selectedColumn != -1 && !(selectedColor.equals(Color.PINK))){
 	    tempName[0][selectedColumn].setBackground(selectedColor);
 	    selectedColumn = -1;
-	    selectedColor = Color.MAGENTA;
+	    selectedColor = Color.PINK;
 	}
         if("CLEAR".equals(e.getActionCommand())){
 	    for (int x = 1; x < 5; x++){
-		tempName[0][x].setBackground(Color.MAGENTA);
+		tempName[0][x].setBackground(Color.PINK);
 	    }
 	    selectedColumn = -1;
-	    selectedColor = Color.MAGENTA;
+	    selectedColor = Color.PINK;
 	}
 	
     }
